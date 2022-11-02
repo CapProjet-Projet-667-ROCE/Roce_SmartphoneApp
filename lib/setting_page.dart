8324ab294964c21d4314f5bc2846f5fa795e2247
blasseye <lasseye@t.esiea.fr>
2022-11-02 16:44:05 +0100

    fuck ca marche plus

--- lib/setting_page.dart
+++ lib/setting_page.dart
@@ -15,19 +15,6 @@ class _SettingPage extends State<SettingPage> {
 
     return Scaffold(
       appBar: SettingAppBar(),
-      body: Column(
-        children: [
-          CommonSetting(),
-          DropDownSettingsTile(
-            title: 'Rotation',
-            selected: 1,
-            values: <int, String>{
-              1: 'vertical',
-              2: 'horizontal',
-            },
-          ),
-        ],
-      ),
     );
   }
 }
@@ -40,45 +27,6 @@ class SettingAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Color(0xFF031319),
       title: Text('Setting'),
       centerTitle: true,
-    );
-  }
-}
-
-class CommonSetting extends StatefulWidget {
-  @override
-  _CommonSetting createState() => _CommonSetting();
-}
-
-class _CommonSetting extends State<CommonSetting> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: SettingsList(
-        sections: [
-          SettingsSection(
-            title: Text('Common'),
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                leading: Icon(Icons.language),
-                title: Text('Language'),
-                value: Text('English'),
-              ),
-              SettingsTile.navigation(
-                leading: Icon(Icons.info),
-                title: Text('Version'),
-                value: Text('0.0.1 BETA'),
-              ),
-              SettingsTile.switchTile(
-                onToggle: (value) {},
-                initialValue: true,
-                leading: Icon(Icons.dark_mode_outlined),
-                title: Text('Enable custom theme'),
-              ),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
