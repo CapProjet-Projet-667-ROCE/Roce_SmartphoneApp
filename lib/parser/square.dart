b3975b991338126f65e0605c19b9c2d6b7d0648a
blasseye <lasseye@t.esiea.fr>
2022-12-14 11:56:37 +0100

    connexion test

--- lib/parser/square.dart
+++ lib/parser/square.dart
@@ -1,30 +1,16 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:roce_smartphoneapp/main.dart';
 
-class DataSquare {
-  int _id;
-  int _color;
-  String _title;
-
-  DataSquare(this._id, this._color, this._title);
-
-  factory DataSquare.fromJSON(Map<String, dynamic> json) {
-    if (json == null) {
-      return DataSquare(0, 0x88031319, "Empty");
-    } else {
-      return DataSquare(json["id"], json["color"], json["title"]);
-    }
-  }
-
-  get id => this._id;
-  get color => this._color;
-  get text => this._title;
-}
+int _rotateValue = 0;
 
 class Square extends StatefulWidget {
-  final int id;
+  final String id;
   final int colorBackground;
   final String textOfSquare;
-  const Square(this.id, this.colorBackground, this.textOfSquare);
+  final Socket socket;
+
+  const Square(this.socket, this.id, this.colorBackground, this.textOfSquare);
   @override
   _Square createState() => _Square();
 }
@@ -40,9 +26,12 @@ class _Square extends State<Square> {
           child: ElevatedButton(
             child: RotatedBox(
               child: Text(widget.textOfSquare),
-              quarterTurns: 0,
+              quarterTurns: _rotateValue,
             ),
-            onPressed: () => print(widget.id),
+            onPressed: () async {
+              sendMessage(widget.socket, widget.id);
+              debugPrint('id: ' + widget.id);
+            },
             style: ElevatedButton.styleFrom(
               primary: Color(widget.colorBackground),
             ),
--- lib/setting.dart
+++ lib/setting.dart
@@ -19,71 +19,70 @@ class SettingBody extends StatefulWidget {
 }
 
 class _SettingBody extends State<SettingBody> {
-  Future<String> getIpAddress() async {
-    final ipv4 = await Ipify.ipv4();
-    return ipv4;
-  }
+  static var keyIpDesktop = 'key-ip-desktop';
+  static var keyPort1Desktop = 'key-port1-desktop';
+  static var keyPort2Desktop = 'key-port2-desktop';
+  static var keyConnexionDesktop = 'key-connexion-desktop';
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            height: 300,
-            child: SettingsGroup(
-              title: 'Connection Settings',
-              children: <Widget>[
-                Container(
-                    child: FutureBuilder(
-                        future: getIpAddress(),
-                        builder:
-                            (BuildContext context, AsyncSnapshot<String> text) {
-                          if (!text.hasData) {
-                            return Container();
-                          } else {
-                            final String? addrIp = text.data;
-                            return Text("Adresse IP : $addrIp");
-                          }
-                        })),
-                TextInputSettingsTile(
-                  title: 'Ip address desktop',
-                  settingKey: 'key-ip-desktop',
-                  initialValue: '192.161.1.', //Affichage VS valeur
-                  borderColor: Colors.blueAccent,
-                  errorColor: Colors.deepOrangeAccent,
-                ),
-                TextInputSettingsTile(
-                  title: 'Port One desktop',
-                  settingKey: 'key-port1-desktop',
-                  initialValue: '0000',
-                  borderColor: Colors.blueAccent,
-                  errorColor: Colors.deepOrangeAccent,
-                ),
-                TextInputSettingsTile(
-                  title: 'Port Two desktop',
-                  settingKey: 'key-port1-desktop',
-                  initialValue: '0000',
-                  borderColor: Colors.blueAccent,
-                  errorColor: Colors.deepOrangeAccent,
-                ),
-                SimpleDropDownSettingsTile(
-                  title: 'Connection with desktop',
-                  settingKey: 'key-connection-desktop',
-                  values: <String>[
-                    'WIFI',
-                    'USB',
-                  ],
-                  selected: 'WIFI',
-                  onChange: (value) {
-                    debugPrint('key-connection-desktop: $value');
-                  },
-                ),
-              ],
-            ),
+    return Column(
+      children: [
+        Container(
+          height: 600,
+          child: SettingsGroup(
+            title: 'Connection Settings',
+            children: <Widget>[
+              TextInputSettingsTile(
+                title: 'Ip address desktop',
+                settingKey: 'key-ip-desktop',
+                initialValue: '192.161.1.', //Affichage VS valeur
+                borderColor: Colors.blueAccent,
+                errorColor: Colors.deepOrangeAccent,
+                onChange: (value) {
+                  keyIpDesktop = value;
+                  debugPrint('key-connexion-desktop: $keyIpDesktop');
+                },
+              ),
+              TextInputSettingsTile(
+                title: 'Port One desktop',
+                settingKey: 'key-port1-desktop',
+                initialValue: '0000',
+                borderColor: Colors.blueAccent,
+                errorColor: Colors.deepOrangeAccent,
+                onChange: (value) {
+                  keyPort1Desktop = value;
+                  debugPrint('key-connexion-desktop: $keyPort1Desktop');
+                },
+              ),
+              TextInputSettingsTile(
+                title: 'Port Two desktop',
+                settingKey: 'key-port2-desktop',
+                initialValue: '0000',
+                borderColor: Colors.blueAccent,
+                errorColor: Colors.deepOrangeAccent,
+                onChange: (value) {
+                  keyPort2Desktop = value;
+                  debugPrint('key-port2-desktop: $keyPort2Desktop');
+                },
+              ),
+              SimpleDropDownSettingsTile(
+                title: 'Connexion with desktop',
+                settingKey: 'key-connexion-desktop',
+                values: <String>[
+                  'WIFI',
+                  'USB',
+                ],
+                selected: 'WIFI',
+                onChange: (value) {
+                  keyConnexionDesktop = value;
+                  debugPrint('key-connexion-desktop: $keyConnexionDesktop');
+                },
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
